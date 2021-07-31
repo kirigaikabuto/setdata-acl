108b3997910242fd59f0097772e762eab90b3577
Yerassyl <tleugazy98@gmail.com>
2021-07-31 11:13:34 +0600

    commits

--- role_amqp_endpoints.go
+++ role_amqp_endpoints.go
@@ -32,3 +32,60 @@ func (r *RoleAmqpEndpoints) MakeCreateRoleAmqpEndpoint() amqp.Handler {
 		return &amqp.Message{Body: jsonResponse}
 	}
 }
+
+func (r *RoleAmqpEndpoints) MakeGetRoleAmqpEndpoint() amqp.Handler {
+	return func(message amqp.Message) *amqp.Message {
+		cmd := &GetRoleCommand{}
+		err := json.Unmarshal(message.Body, &cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		response, err := r.ch.ExecCommand(cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		return &amqp.Message{Body: jsonResponse}
+	}
+}
+
+func (r *RoleAmqpEndpoints) MakeListRoleAmqpEndpoint() amqp.Handler {
+	return func(message amqp.Message) *amqp.Message {
+		cmd := &ListRoleCommand{}
+		err := json.Unmarshal(message.Body, &cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		response, err := r.ch.ExecCommand(cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		return &amqp.Message{Body: jsonResponse}
+	}
+}
+
+func (r *RoleAmqpEndpoints) MakeDeleteRoleAmqpEndpoint() amqp.Handler {
+	return func(message amqp.Message) *amqp.Message {
+		cmd := &DeleteRoleCommand{}
+		err := json.Unmarshal(message.Body, &cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		response, err := r.ch.ExecCommand(cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		return &amqp.Message{Body: jsonResponse}
+	}
+}
