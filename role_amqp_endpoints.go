f8a5a95121761e9f140e0ff916f2c9fcc83763c7
Yerassyl <tleugazy98@gmail.com>
2021-07-31 11:01:35 +0600

    commits

--- role_amqp_endpoints.go
+++ role_amqp_endpoints.go
@@ -1,2 +1,34 @@
 package setdata_acl
 
+import (
+	"encoding/json"
+	"github.com/djumanoff/amqp"
+	setdata_common "github.com/kirigaikabuto/setdata-common"
+)
+
+type RoleAmqpEndpoints struct {
+	ch setdata_common.CommandHandler
+}
+
+func NewRoleAmqpEndpoints(ch setdata_common.CommandHandler) RoleAmqpEndpoints {
+	return RoleAmqpEndpoints{ch: ch}
+}
+
+func (r *RoleAmqpEndpoints) MakeCreateRoleAmqpEndpoint() amqp.Handler {
+	return func(message amqp.Message) *amqp.Message {
+		cmd := &CreateRoleCommand{}
+		err := json.Unmarshal(message.Body, &cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		response, err := r.ch.ExecCommand(cmd)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		jsonResponse, err := json.Marshal(response)
+		if err != nil {
+			return setdata_common.ErrToAmqpResponse(err)
+		}
+		return &amqp.Message{Body: jsonResponse}
+	}
+}
